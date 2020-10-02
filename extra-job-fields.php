ea039055c2507071738567f3ad046133a39e6dd9
Mithun Biswas <bhoot.biswas@gmail.com>
2020-10-02 14:32:45 +0600

    Add initial files

---
+++ extra-job-fields.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Extra Job Fields
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     extra-job-fields
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Extra_Job_Fields
+ */
+
+// Your code starts here.
